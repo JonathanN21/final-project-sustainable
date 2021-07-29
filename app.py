e7ec6910ff686aad12dd468fbc68e83633f15b21
Jonathan Ngo <jonathann2142@gmail.com>
2021-07-29 13:47:13 +0000

    first

--- app.py
+++ app.py
@@ -18,17 +18,27 @@ app.jinja_env.globals['current_time'] = dt.datetime.now()
 @app.route('/index')
 def index():
     data = {
+        "ingredients":model.dict_food
     }
-    return render_template('index.html', data=model.dict_food)
+    
+    return render_template('index.html', data=data)
 
 @app.route('/convert',methods=["POST","GET"])
 def convertC02emit():
     form = request.form
     kilos_list = request.form.getlist('kilos')
     print(kilos_list)
-    carbonprint = model.convert_food_to_CO2(form["food"],form["kilos"])
-    data = {"CO2":carbonprint}
+    carbonprint = 0
+    i = 0
+    for food in model.dict_food:
+        carbonprint += model.convert_food_to_CO2(food,kilos_list[i])
+        i+=1
+    data = {
+        "CO2":carbonprint, 
+        "ingredients":model.dict_food
+        }
     print(data)
-    return render_template('results.html', data=data)
+    return render_template('index.html', data=data)
+
 
 
